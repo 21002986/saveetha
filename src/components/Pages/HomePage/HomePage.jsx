51b7bb67659d254eaf05b769cff8a11bfc124587
saivinay <kanamarlapudisaivinay@gmail.com>
2024-07-21 21:37:52 +0530

    Comn

---
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import "./HomePage.css";
+export default function HomePage(){
+    return(
+        <>
+        <div className="homepage">
+        <h1>Welcome to Our College</h1>
+        <img src="https://cache.careers360.mobi/media/article_images/2023/5/26/Saveetha-Engineering-College-admissions-featured-image.jpg"></img>
+            </div>
+         
+   
+    </>
+    )
+}
